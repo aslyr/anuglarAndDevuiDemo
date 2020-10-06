fa7cc2f19b3a4ef0d6f097966fd6d1a2694906a2
380234169 <380234169@qq.com>
2020-10-06 23:35:14 +0800

    第一次提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AccordionItemClickEvent, IFileOptions, IUploadOptions} from 'ng-devui';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App1';
+  fileOptions: IFileOptions = {
+    multiple: true,
+    accept: '.*',
+  };
+  uploadedFiles: Array<object> = [];
+  uploadedFiles1: Array<object> = [];
+  uploadOptions: IUploadOptions = {
+    uri: '/upload',
+    headers: {},
+    maximumSize: 1000,
+    method: 'POST',
+    fileFieldName: 'dFile'
+  };
+  onSuccess(result){
+    console.log(result);
+  }
+
+  onChange(event){
+    console.log(event);
+  }
+  logoSrc = 'https://res.hc-cdn.com/x-roma-components/1.0.10/assets/devui/logo.svg';
+  menu = [
+    {
+      title: '内容一',
+      open: true,
+      children: [{ title: '子内容1', link: 'http://www.baidu.com' , linkType: 'hrefLink' }, { title: '子内容2' }, { title: '子内容3' }],
+    },
+    {
+      title: '内容二',
+      children: [{ title: '子内容1' }, { title: '子内容2' }, { title: '子内容3' }],
+    },
+    {
+      title: '内容三（默认展开）',
+      open: true,
+      children: [{ title: '子内容1(禁用)', disabled: true }, { title: '子内容2(默认激活)', active: true }, { title: '子内容3' }],
+    },
+  ];
+
+  itemClick(event: AccordionItemClickEvent) {
+    console.log(event.item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DevUIModule} from 'ng-devui';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DevUIModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
